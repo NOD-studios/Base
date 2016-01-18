d32f076be85675bedec134535053c5f4a3fef828
Furkan Tunalı <furkan@nod.st>
2016-01-18 18:25:35 +0200

    1.2.2

--- dist/commonjs/instance.js
+++ dist/commonjs/instance.js
@@ -1,14 +1,11 @@
-"use strict";
+define(["exports", "./base", "babel-polyfill"], function (exports, _base) {
+  "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.base = undefined;
+  var base = exports.base = new _base.Base();
+  exports.default = base;
 });
-exports.base = undefined;
-
-require("babel-polyfill");
-
-var _base = require("./base");
-
-var base = exports.base = new _base.Base();
-exports.default = base;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluc3RhbmNlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBRU8sSUFBSSxJQUFJLFdBQUosSUFBSSxHQUFHLFVBRFQsSUFBSSxFQUNlLENBQUM7a0JBQ2QsSUFBSSIsImZpbGUiOiJpbnN0YW5jZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBcImJhYmVsLXBvbHlmaWxsXCI7XG5pbXBvcnQgeyBCYXNlIH0gZnJvbSAnLi9iYXNlJztcbmV4cG9ydCBsZXQgYmFzZSA9IG5ldyBCYXNlKCk7XG5leHBvcnQgZGVmYXVsdCBiYXNlO1xuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluc3RhbmNlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7TUFFVyxJQUFJLFdBQUosSUFBSSxHQUFHLFVBRFQsSUFBSSxFQUNlO29CQUNiLElBQUkiLCJmaWxlIjoiaW5zdGFuY2UuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgXCJiYWJlbC1wb2x5ZmlsbFwiO1xuaW1wb3J0IHsgQmFzZSB9IGZyb20gJy4vYmFzZSc7XG5leHBvcnQgbGV0IGJhc2UgPSBuZXcgQmFzZSgpO1xuZXhwb3J0IGRlZmF1bHQgYmFzZTtcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
